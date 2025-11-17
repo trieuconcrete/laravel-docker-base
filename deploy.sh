3f87a5382533a544bdc04d12f80d136e4c6f84e7
trieuconcrete <trieunb@concrete-corp.com>
2025-11-17 21:12:44 +0700

    add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ git pull origin Master
 cd src
 composer install --no-dev
 php artisan migrate --force
+php artisan config:clear
+php artisan cache:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
